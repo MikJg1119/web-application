753e3425bc8915e0446b45955e0dc9516c493010
Mikolaj Jagiello <mikigraf@wp.pl>
2018-06-23 16:05:22 +0200

    User-editing forms (pre-working)

--- src/main/java/controller/UserEditServlet.java
+++ src/main/java/controller/UserEditServlet.java
@@ -1,5 +1,8 @@
 package controller;
 
+import dao.UserDao;
+import model.User;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,13 +10,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/users/edit")
 public class UserEditServlet extends HttpServlet {
+
+    private UserDao userDao;
+    public UserEditServlet (){
+        this.userDao = new UserDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/editform.jsp");
+        int id = Integer.parseInt(req.getParameter("id"));
 
-        rd.forward(req,resp);
+        try {
+            User user = userDao.findById(id);
+            RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/editform.jsp");
+
+            rd.forward(req,resp);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
     }
 }
